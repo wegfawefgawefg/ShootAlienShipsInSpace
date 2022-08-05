2edd4b7a5e572bc5a86958bce2421d93075d44d6
wegfawefgawefg <668es218pur@gmail.com>
2022-08-05 18:47:01 +0900

    cage in

--- main.py
+++ main.py
@@ -187,16 +187,17 @@ class Bullet:
 
 class KillZones:
     def __init__(self) -> None:
-        self.right_shape = self.make_body((50, 0), (70, SCREEN_DIMS.y))
-        self.left_shape = self.make_body((0, 0), (20, SCREEN_DIMS.y))
-        right_shape_x = SCREEN_DIMS.x - 10
-
-        self.back_shape = self.make_body((0, 1), (SCREEN_DIMS.x, 1))
+        self.right_shape = self.make_body(
+            (SCREEN_DIMS.x, 0), (SCREEN_DIMS.x, SCREEN_DIMS.y))
+        self.left_shape = self.make_body((-1, 0), (-1, SCREEN_DIMS.y))
+        self.back_shape = self.make_body((0, 0), (SCREEN_DIMS.x, 0))
+        self.front_shape = self.make_body(
+            (0, SCREEN_DIMS.y), (SCREEN_DIMS.x, SCREEN_DIMS.y))
 
     def make_body(self, a, b):
         line_moment = pymunk.moment_for_segment(0, a, b, 10)
         line_body = pymunk.Body(1, line_moment, body_type=pymunk.Body.STATIC)
-        line_body.position = a
+        # line_body.position = a
         line_shape = pymunk.Segment(line_body, a, b, 1)
         PHYSWORLD.add(line_body, line_shape)
         return line_shape
@@ -213,9 +214,11 @@ class KillZones:
         )
 
     def draw(self):
-        self.draw_line(self.left_shape)
-        self.draw_line(self.right_shape)
-        self.draw_line(self.back_shape)
+        pass
+        # self.draw_line(self.left_shape)
+        # self.draw_line(self.right_shape)
+        # self.draw_line(self.back_shape)
+        # self.draw_line(self.front_shape)
 
 
 class Ship:
